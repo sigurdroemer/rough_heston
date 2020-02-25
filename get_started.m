4e517c031cb47bd142f112d923b479ed83acefbd
Sigurd Emil Rømer <50485381+sigurdroemer@users.noreply.github.com>
2020-02-25 14:05:17 +0100

    Update get_started.m

--- get_started.m
+++ get_started.m
@@ -2,7 +2,7 @@
 projFolder = fileparts(matlab.desktop.editor.getActiveFilename);
 addpath(genpath(projFolder));
 
-%% Example 1 (A single smiles):
+%% Example 1 (A single smile):
 v_0 = 0.15.^2;v_bar=0.15.^2;alpha = 0.6;lambda = 2;xi = .4;rho = -0.6;
 s0 = 100;K = (50:5:150)';T = 1;call = true;
 [price, iv] = NumericalIntegrationRoughHeston(s0,v_0,alpha,lambda,...
